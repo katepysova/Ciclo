56ffcb2913759992002088c8de6fd1c8c043c1dd
Kateryna <katepysova97@gmail.com>
2023-07-26 20:22:18 +0200

    Added fullpage scroll logic

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+import FullPageObserver from "./fullpage.js";
+
+const sections = document.querySelectorAll(".fullpage-section");
+const SCROLL_TRESHOLD = 0.25;
+
+sections.forEach((section) => {
+  const observer = new FullPageObserver(
+    section,
+    SCROLL_TRESHOLD,
+    (target) => {
+      target.classList.add("is-visible");
+    },
+    (target) => {
+      target.classList.remove("is-visible");
+    }
+  );
+  observer.observe();
+});
